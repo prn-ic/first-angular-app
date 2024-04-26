3654bfd90d094a7e5586813ae9c19c57bc60dab9
prn-ic <domarlaid2@mail.ru>
2024-04-26 13:28:36 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterModule, HomeComponent],
+  template: `<main>
+              <a [routerLink]='["/"]'>
+              <header class='brand-name'>
+                <img class='brand-logo' src='/assets/logo.svg' alt='logo' aria-hidden='true'/>
+              </header>
+              </a>
+              <section class='content'>
+                <router-outlet></router-outlet>
+              </section>
+            </main>`,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'life-cycles-app';
+  countExternal = 0;
+  plus() {
+    this.countExternal += 1;
+  }
+  title = 'homes';
 }
---
+++ src/app/components/home/home/home.component.ts
@@ -0,0 +1,57 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocationComponent } from '../../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../../housing.service';
+import { HousingLocation } from '../../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="
+          let housingLocation of filteredLocationList"
+
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((hl) =>
+      hl?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
